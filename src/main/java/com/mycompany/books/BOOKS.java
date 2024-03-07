a82c1a575d316853b0e89d4e653c364caba21999
Keinerballesteros <kballesterosm@ufpso.edu.co>
2024-03-06 21:01:52 -0500

    load

---
+++ src/main/java/com/mycompany/books/BOOKS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.books;
+
+/**
+ *
+ * @author DELL
+ */
+public class BOOKS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
